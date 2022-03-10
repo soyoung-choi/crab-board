0fbb38aed2aad877fdeacef94490a2dc181f9f9e
soyoung-choi <sydesign24@naver.com>
2022-03-10 14:53:36 +0900

    axios 인터셉터 수정

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
+import { getAuthFromCookie } from '@/utils/cookies'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5001/api',
+	baseURL: process.env.VUE_APP_API_URL,
 	timeout: 1000,
 })
 
 instance.interceptors.request.use(
 	function (config) {
-		// 요청 바로 직전
-		// axios 설정값에 대해 작성합니다.
+		// 요청 바로 직전 axios 설정값 작성
+		const access_token = getAuthFromCookie()
+		if (access_token) {
+			config.headers.Authorization = `Bearer ${access_token}`
+		}
 		return config
 	},
 	function (error) {
